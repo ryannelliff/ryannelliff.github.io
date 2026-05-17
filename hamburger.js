f1a3a4f04558983a9d5a3e54d5f54142648c23f2
Ryann Elliff <reelliff@gmail.com>
2026-05-16 22:47:14 -0500

    Big conversion from react

---
+++ hamburger.js
@@ -0,0 +1,11 @@
+// =========================
+// Mobile Navbar Toggle
+// =========================
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
